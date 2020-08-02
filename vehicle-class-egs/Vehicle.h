6af67fcd3e8a79f40ee427d99d1c6aa1b2afa39a
dym4xion <dym4xion@users.noreply.github.com>
2020-08-02 15:20:40 +0100

    demo of inheritance and virtual functions. see vehicle eg

--- vehicle-class-egs/Vehicle.h
+++ vehicle-class-egs/Vehicle.h
@@ -1,6 +1,7 @@
 #include <iostream>
 using namespace std;
 
+// attributes common to any combustion engine vehicle
 class Vehicle {
     // inst vars
     int nPass;
@@ -8,15 +9,15 @@ class Vehicle {
     int mpg;
 public:
     // con/des
-    Vehicle (int numPassengers, int fuelCapacity, int fuelRate);
-    Vehicle ();
+    Vehicle(int numPassengers, int fuelCapacity, int fuelRate);
+    Vehicle();
     ~Vehicle();
     // accessors
     int get_num_passengers() { return nPass; }
     int get_fuel_capacity() { return fCap; }
     int get_num_mpg() { return mpg; }
     // other methods
-    int range();
+    virtual int range() {return fCap * mpg;}
 };
 
 Vehicle::Vehicle (int numPassengers, int fuelCapacity, int fuelRate){
@@ -35,4 +36,41 @@ Vehicle::~Vehicle(){
     cout << "destructing" << endl;
 }
 
-int Vehicle::range() {return fCap * mpg;}
+class Plane : public Vehicle {
+    public:
+        int ws;
+        Plane(int wingspan);
+        ~Plane();
+        int getWingspan();
+};
+
+Plane::Plane(int wingspan) : Vehicle(){
+    ws = wingspan;
+}
+
+Plane::~Plane(){
+    cout << "destructing a plane" << endl;
+}
+
+int Plane::getWingspan(){
+    return ws;
+}
+
+class JetPlane : public Plane {
+    public:
+        int ws;
+        int air;
+        JetPlane(int wingspan, int airIntakeRate);
+        ~JetPlane();
+        int getAir() { return air; }
+        int range() { return (get_fuel_capacity()*get_num_mpg()) / 2 + 30; } // completely arbitrary and nonsensical formula used to demo virtual methods without having to write constructors for plane and jet plane that also take passenger capacity, fuel capacity, and mpg...
+};
+
+JetPlane::JetPlane(int wingspan, int airIntakeRate) : Plane(wingspan){
+    air = airIntakeRate;
+}
+
+JetPlane::~JetPlane(){
+    cout << "destructing a jet plane" << endl;
+}
+// did you know that a derived class can have multiple base classes...
--- vehicle-class-egs/driver.cpp
+++ vehicle-class-egs/driver.cpp
@@ -15,5 +15,21 @@ int main(){
     cout << "car has a fuel rate of " << car.get_num_mpg() << " miles per gallon" << endl;
     cout << "car has range of " << car.range() << " miles" << endl;
     
+
+    Plane p = Plane(55);
+    cout << "plane has " << p.get_num_passengers() << " passengers" << endl;
+    cout << "plane has fuel capacity of " << p.get_fuel_capacity() << " gallons" << endl;
+    cout << "plane has a fuel rate of " << p.get_num_mpg() << " miles per gallon" << endl;
+    cout << "plane has range of " << p.range() << " miles" << endl;
+    cout << "plane has wingspan of " << p.getWingspan() << " metres" << endl;
+    
+    JetPlane jp = JetPlane(55, 1000);
+    cout << "jet plane has " << jp.get_num_passengers() << " passengers" << endl;
+    cout << "jet plane has fuel capacity of " << jp.get_fuel_capacity() << " gallons" << endl;
+    cout << "jet plane has a fuel rate of " << jp.get_num_mpg() << " miles per gallon" << endl;
+    cout << "jet plane has range of " << jp.range() << " miles" << endl;
+    cout << "jet plane has wingspan of " << jp.getWingspan() << " metres" << endl;
+    cout << "jet plane has engine air intake rate of " << jp.getAir() << " kg/s" << endl;
+    
     return 0;
 }
